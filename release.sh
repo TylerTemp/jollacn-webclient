4d61512824a4a138b1fc01cafa8e94ed00c93294
TylerTemp <tylertempdev@gmail.com>
2024-02-06 11:06:50 +0000

    update script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 yarn build
 ssh tyler@notexists.top 'mkdir -p release/jollacn-webclient'
-scp -r build/* tyler@notexists.top:release/jollacn-webclient
+scp -r build/* tyler@notexists.top:/var/www/notexists_top
